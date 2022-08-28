2b0a416ec95032be057447e7aa07f150986c4738
luiz <luizvinicius.emprego@hotmail.com>
2022-08-27 21:19:13 -0300

    create server

---
+++ app.py
@@ -0,0 +1,12 @@
+#Flask
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
+
+#rota
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Executar app
+if __name__ == '__main__':
+    app.run()
